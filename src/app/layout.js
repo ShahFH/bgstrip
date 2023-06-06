39ba1bf1303f99a6d3e0e4d75f4f699b299d34ac
ShahFH <shahfaisal.swe@example.com>
2023-06-06 22:11:56 +0430

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
       <body className={manrope.className}>
       <Navbar/>
       {children}
-      
       <Footer/>
       </body>
     </html>
